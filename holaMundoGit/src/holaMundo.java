9cf83866f3a37ba6a507ae558a1a503b14f1642f
Usuario <Usuario@Usuario-PC>
2013-03-04 13:11:26 +0000

    Nombre

--- holaMundoGit/src/holaMundo.java
+++ holaMundoGit/src/holaMundo.java
@@ -4,9 +4,14 @@ public class holaMundo {
 	/**
 	 * @param args
 	 */
+	String nombre;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.printf("Probando Probando");
+	}
+	
+	public String getNombre(String nom){
+		this nombre = nom;
 	}
 
 }
